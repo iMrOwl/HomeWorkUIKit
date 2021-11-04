115a67819a844d17fec9543ea484df9a3ea2b78d
Евгений Сычёв <sycheved@msk.bcs.ru>
2021-11-04 11:40:07 +0300

    Initial Commit

---
+++ HomeWorkUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkUIKit
+//
+//  Created by Евгений Сычёв on 04.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
